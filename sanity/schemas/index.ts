4f587783677d0497910dc15b098412872711a385
Matthew Bishop <mattbishopfx@gmail.com>
2025-10-19 14:53:53 -0500

    Remove Sanity Studio for production build

--- sanity/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import blogPost from './blogPost';
-import infoCard from './infoCard';
-import achievement from './achievement';
-import service from './service';
-
-export const schemaTypes = [blogPost, infoCard, achievement, service];
-
